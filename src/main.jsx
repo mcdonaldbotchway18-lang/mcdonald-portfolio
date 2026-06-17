5236fb27101166cac04dbb80975540e4e1c59d02
mcdonaldbotchway18-lang <mcdonaldbotchway18@gmail.com>
2026-06-17 09:56:33 +0000

    Delete src directory

--- src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import Portfolio from "./Portfolio.jsx";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <Portfolio />
-  </React.StrictMode>
-);
