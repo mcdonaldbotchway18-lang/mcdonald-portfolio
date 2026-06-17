358d04040df4acf46d9c3cecabbdafc23bb69743
mcdonaldbotchway18-lang <mcdonaldbotchway18@gmail.com>
2026-06-17 10:05:58 +0000

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import LuxuryPortfolio from "../LuxuryPortfolio.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <LuxuryPortfolio />
+  </React.StrictMode>
+);
